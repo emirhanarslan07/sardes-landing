5eb9081bca33746edfba0f36016dbc937fc6433d
Emirhan Arslan <emirhanarslan625@gmail.com>
2025-12-22 09:43:45 +0300

    Activate Google Analytics G-JQVBNRB1N6 - Enable tracking and form conversions

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { LanguageProvider } from "./contexts/LanguageContext";
-// import { initGA } from "./lib/analytics"; // TEMPORARILY DISABLED
+import { initGA } from "./lib/analytics";
 
-// Initialize Google Analytics - TEMPORARILY DISABLED
-// initGA();
+// Initialize Google Analytics
+initGA();
 
 createRoot(document.getElementById("root")!).render(
   <LanguageProvider>
