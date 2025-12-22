3bae3e3709438cb90a2265819facc21762c4cd1a
Emirhan Arslan <emirhanarslan625@gmail.com>
2025-12-22 08:11:55 +0300

    Completely disable analytics initialization to fix gtag error

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { LanguageProvider } from "./contexts/LanguageContext";
-import { initGA } from "./lib/analytics";
+// import { initGA } from "./lib/analytics"; // TEMPORARILY DISABLED
 
-// Initialize Google Analytics
-initGA();
+// Initialize Google Analytics - TEMPORARILY DISABLED
+// initGA();
 
 createRoot(document.getElementById("root")!).render(
   <LanguageProvider>
